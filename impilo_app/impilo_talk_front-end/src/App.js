313d16df97ecd2c08168a24c19e5701c0694bdf8
MamaKbubu <maboelatk@gmail.com>
2024-07-18 21:04:56 +0200

    front end updated

--- impilo_app/impilo_talk_front-end/src/App.js
+++ impilo_app/impilo_talk_front-end/src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import SignInPage from "./pages/signin";
+import About from "./pages/About";
+import Testimonials from "./pages/Testimonials";
 
 function App() {
   return (
-    <>
-      <h1>hey B1tch</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+      </Routes>
+    </Router>
   );
 }
 
